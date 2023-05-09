c82da6418fd7f2585b700f4dafca2e99bf4a381b
Pedryn <ph9530338@gmail.com>
2023-05-09 18:44:38 -0300

    3 primeiras entregas corrigido

--- Entrega 3/app.py
+++ Entrega 3/app.py
@@ -1,64 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
 from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
-app.config['MYSQL_host'] = 'localhost'
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'fatec'
-app.config['MYSQL_DB'] = 'Faculdade'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
 
 
 @app.route("/")
 def home():
-    titulo = "Home"
-    return render_template ("home.html", title = titulo)
-
-@app.route("/home")
-def home2():
-    titulo = "Home"
-    return render_template ("home.html", title = titulo)
+    return render_template("home.html")
 
 @app.route("/quemsomos")
-def quemsomos():
-    titulo = "Quem Somos"
-    return render_template ("quemsomos.html", title = titulo)
+def quem_somos():
+    return render_template("quemsomos.html")
 
-@app.route("/contato", methods=['GET', 'POST'])
+@app.route('/contato', methods=['GET', 'POST'])
 def contato():
-    if request.method =='POST':
+    if request.method == "POST":
         email = request.form['email']
-        assunto = request.form['email']
+        assunto = request.form['assunto']
         descricao = request.form['descricao']
-
+        
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO contato(email, assunto, descricao) VALUES (%s, %s, %s)",(email, assunto, descricao))
-
+        cur.execute("INSERT INTO contato (email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
         mysql.connection.commit()
-
+        
         cur.close()
 
         return 'sucesso'
-    return render_template("contato.html")
+    return render_template('contato.html')
 
-#@app.route("/contato")
-#def contato():
-#    titulo = "Contato"
-#    return render_template ("contato.html", title = titulo)
 
-# rota usuários para listar todos os usuários no banco de dados
-
+# rota usuários para listar todos os usuário no banco de dados.
 @app.route('/users')
 def users():
     cur = mysql.connection.cursor()
 
     users = cur.execute("SELECT * FROM contato")
-     if users > 0:
+
+    if users > 0:
         userDetails = cur.fetchall()
 
-        return_template("user.html", userDetails=userDetails)
-
-#testa ve se é aqui mesmo ou em cima do comentado acima
+        return render_template("users.html", userDetails=userDetails)
 
 if __name__ == '__main__':
     app.run()
